dd39c26b9c1bc8b69c3887b9fbb1163a9cd2771b
TracyNuwagaba <tnuwagaba191@gmail.com>
2021-03-24 04:54:38 +0300

    built palindrome checker function

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
+// Checking for a palindrome using built-in functions
+
 function palindrome(str) {
-    return true;
+    let reg = /[\W_]/g;
+    /* Step 1. Lowercase the string and use the regex to remove unwanted
+    characters from it */
+    let lowerCaseStr = str.toLowerCase().replace(reg, '');
+    // Step 2. Use built-in functions to reverse the string
+    let reversedStr = lowerCaseStr.split('').reverse().join('');
+    /* Step 3. Check if reversedStr is strictly equals to lowerCasestr
+    and returns a boolean */
+    return reversedStr === lowerCaseStr;
 }
 
-palindrome("eye");
+console.log(palindrome("A man, a plan, a canal. Panama"));
