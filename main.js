bd9be4e064a11454bc9af2384839db41d1573d7f
TracyNuwagaba <tnuwagaba191@gmail.com>
2021-03-25 05:45:48 +0300

    build a roman numeral converter

--- main.js
+++ main.js
@@ -1,15 +1,54 @@
-// Checking for a palindrome using built-in functions
+/* Convert the given number into a roman numeral
+All roman numerals answers should be provided in upper case */
 
-function palindrome(str) {
-    let reg = /[\W_]/g;
-    /* Step 1. Lowercase the string and use the regex to remove unwanted
-    characters from it */
-    let lowerCaseStr = str.toLowerCase().replace(reg, '');
-    // Step 2. Use built-in functions to reverse the string
-    let reversedStr = lowerCaseStr.split('').reverse().join('');
-    /* Step 3. Check if reversedStr is strictly equals to lowerCasestr
-    and returns a boolean */
-    return reversedStr === lowerCaseStr;
+function convertToRoman(num) {
+    let romanNumeral = "";
+
+    // Using a while loop with if statements
+    while (num !== 0) {
+        if (num >= 1000) {
+            romanNumeral += 'M';
+            num -= 1000;
+        } else if (num >= 900) {
+            romanNumeral += 'CM';
+            num -= 900;
+        } else if (num >= 500) {
+            romanNumeral += 'D';
+            num -= 500;
+        } else if (num >= 400) {
+            romanNumeral += 'CD';
+            num -= 400;
+        } else if (num >= 100) {
+            romanNumeral += 'C';
+            num -= 100;
+        } else if (num >= 90) {
+            romanNumeral += 'XC';
+            num -= 90;
+        } else if (num >= 50) {
+            romanNumeral += 'L';
+            num -= 50;
+        } else if (num >= 40) {
+            romanNumeral += 'XL';
+            num -= 40;
+        } else if (num >= 10) {
+            romanNumeral += 'X';
+            num -= 10;
+        } else if (num >= 9) {
+            romanNumeral += 'IX';
+            num -= 9;
+        } else if (num >= 5) {
+            romanNumeral += 'V';
+            num -= 5;
+        } else if (num >= 4) {
+            romanNumeral += 'IV';
+            num -= 4;
+        } else {
+            romanNumeral += 'I';
+            num -= 1;
+        }
+    }
+
+    console.log(romanNumeral);
 }
 
-palindrome("A man, a plan, a canal. Panama");
+convertToRoman(36);
